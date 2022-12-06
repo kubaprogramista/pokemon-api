efdb0a48c8634fac25019ae0a1424795807d158b
jacod <jakubcoding@gmail.com>
2022-12-06 15:56:03 +0100

    css

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+const nameContainer = document.querySelector(".pokemon-name");
+const idContainer = document.querySelector(".pokemon-id");
 const imageContainer = document.querySelector(".image");
 //total pokemons: 898
 const randomID = Math.floor(Math.random() * 898);
@@ -17,6 +19,9 @@ fetch(`${pokemonURL}`, {
     let types = data.types;
     let height = (data.height * 0.0254).toFixed(1);
     let weight = (data.weight * 0.45359237).toFixed(1);
+
+    nameContainer.innerHTML = `${name}`;
+    idContainer.innerHTML = `#${id}`;
     imageContainer.innerHTML = `<img src="${image}">`;
     console.log(types[0].type.name);
     if(types[1]){
