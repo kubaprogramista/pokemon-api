ae206112fe9c21f7956bdf83f4ec330f6ab9097e
jacod <jakubcoding@gmail.com>
2022-12-06 21:31:26 +0100

    02

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const nameContainer = document.querySelector(".pokemon-name");
 const idContainer = document.querySelector(".pokemon-id");
 const imageContainer = document.querySelector(".image");
+const movesList = document.querySelector(".moves-list");
 //total pokemons: 898
 const randomID = Math.floor(Math.random() * 898);
 let pokemonURL = `https://pokeapi.co/api/v2/pokemon/${randomID}`
@@ -19,13 +20,22 @@ fetch(`${pokemonURL}`, {
     let types = data.types;
     let height = (data.height * 0.0254).toFixed(1);
     let weight = (data.weight * 0.45359237).toFixed(1);
+    let abilities = data.abilities;
 
     nameContainer.innerHTML = `${name}`;
     idContainer.innerHTML = `#${id}`;
     imageContainer.innerHTML = `<img src="${image}">`;
-    console.log(types[0].type.name);
-    if(types[1]){
-        console.log(types[1].type.name);
-    }
+
+    // console.log(types[0].type.name);
+    // if(types[1]){
+    //     console.log(types[1].type.name);
+    // }
+
+    abilities.forEach(ability => {
+        const newItem = document.createElement("li");
+        newItem.innerHTML = `${ability.ability.name}`;
+        movesList.appendChild(newItem);
+    });
+
     console.log(data);
 } );
