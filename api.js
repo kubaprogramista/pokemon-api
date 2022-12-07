338c54f9b676990e12a134b5a770518b8f8a8545
jacod <jakubcoding@gmail.com>
2022-12-07 02:04:40 +0100

    04 images updated

--- api.js
+++ api.js
@@ -2,10 +2,11 @@ const nameContainer = document.querySelector(".pokemon-name");
 const idContainer = document.querySelector(".pokemon-id");
 const imageContainer = document.querySelector(".image");
 const movesList = document.querySelector(".moves-list");
-const typeContainer = document.querySelector(".type");
+const typeContainer = document.querySelector(".types");
 
 //total pokemons: 898
-const randomID = Math.floor(Math.random() * 898);
+//total images: 809
+const randomID = Math.floor(Math.random() * 809);
 let pokemonURL = `https://pokeapi.co/api/v2/pokemon/${randomID}`
 
 fetch(`${pokemonURL}`, {
@@ -16,9 +17,18 @@ fetch(`${pokemonURL}`, {
 })
 .then(res => res.json())
 .then(function(data) {
-    let image = data.sprites.front_default;
-    let name = data.species.name;
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    let name = capitalizeFirstLetter(data.species.name);
     let id = data.id;
+    if(id < 100) {
+        id = `0${id}`
+    } 
+    if(id < 10){
+        id = `0${id}`
+    }
+    let imageLink = `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${id}.png`;
     let types = data.types;
     let height = (data.height * 0.0254).toFixed(1);
     let weight = (data.weight * 0.45359237).toFixed(1);
@@ -26,7 +36,7 @@ fetch(`${pokemonURL}`, {
 
     nameContainer.innerHTML = `${name}`;
     idContainer.innerHTML = `#${id}`;
-    imageContainer.innerHTML = `<img src="${image}">`;
+    imageContainer.innerHTML = `<img src="${imageLink}">`;
 
     types.forEach(type => {
         const newType = document.createElement("div");
@@ -41,6 +51,6 @@ fetch(`${pokemonURL}`, {
         newItem.innerHTML = `${ability.ability.name}`;
         movesList.appendChild(newItem);
     });
-    console.log(types);
+
     console.log(data);
 } );
