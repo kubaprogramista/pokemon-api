bccb2e092b8634afa57306c181ebf8fc2a1e24f7
jacod <jakubcoding@gmail.com>
2022-12-07 03:46:23 +0100

    colors changing on different type

--- api.js
+++ api.js
@@ -1,8 +1,30 @@
+const contentContainer = document.querySelector(".content");
+const transparentContainer = document.querySelector(".transparent");
 const nameContainer = document.querySelector(".pokemon-name");
 const idContainer = document.querySelector(".pokemon-id");
 const imageContainer = document.querySelector(".image");
 const movesList = document.querySelector(".moves-list");
 const typeContainer = document.querySelector(".types");
+
+const typeColors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	water: '#DEF3FD',
+	electric: '#FCF7DE',
+    ice: '#83ceca',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+    steel: '#606060',
+	fairy: '#fceaff',
+    ghost: '#431661',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+};
 
 //total pokemons: 898
 //total images: 809
@@ -39,9 +61,12 @@ fetch(`${pokemonURL}`, {
     imageContainer.innerHTML = `<img src="${imageLink}">`;
 
     types.forEach(type => {
+        let typeName = type.type.name;
         const newType = document.createElement("div");
         newType.className = `type`;
-        newType.innerHTML = `${type.type.name}`;
+        newType.innerHTML = `${typeName}`;
+        contentContainer.style.background = `${typeColors[typeName]}`;
+        transparentContainer.style.background = `${typeColors[typeName]}`;
         typeContainer.appendChild(newType);
     });
 
